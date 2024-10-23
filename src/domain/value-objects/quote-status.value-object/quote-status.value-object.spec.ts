30148d75dba776f43228708a0d508bdc2e34801b
Alex Meregildo <alexmeco95@gmail.com>
2024-10-23 01:10:03 -0500

    init api_quotes/generate]{codeproject}

--- src/domain/value-objects/quote-status.value-object/quote-status.value-object.spec.ts
+++
@@ -1,7 +0,0 @@
-import { QuoteStatusValueObject } from './quote-status.value-object';
-
-describe('QuoteStatusValueObject', () => {
-  it('should be defined', () => {
-    expect(new QuoteStatusValueObject()).toBeDefined();
-  });
-});
